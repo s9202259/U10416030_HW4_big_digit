9146868a5924a9f4eb0222cda29c7bf71592a801
s9202259 <s9202259@yahoo.com.tw>
2016-04-04 18:45:12 +0800

    創建程式
    程式最初的架構

---
+++ BigNumber.java
@@ -0,0 +1,19 @@
+//U10416030 陳子勤
+
+import java.util.*;
+import java.lang.*;
+
+public class BigNumber {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter two number: ");		
+		int number1 = input.nextInt();
+		int number2 = input.nextInt();
+		
+		System.out.println(number1 + " " + number2);
+	}
+}
+
+class BigDigit {
+}
