eeb3f0def51dee2fff3efae4254062aefd10c402
s9202259 <s9202259@yahoo.com.tw>
2016-04-04 18:58:27 +0800

    新增method
    新增setArray來設定array1 及 array2

--- BigNumber.java
+++ BigNumber.java
@@ -16,4 +16,23 @@ public class BigNumber {
 }
 
 class BigDigit {
+	ArrayList<Integer> array1 = new ArrayList<Integer>();
+	ArrayList<Integer> array2 = new ArrayList<Integer>();
+	ArrayList<Integer> arrayPlus = new ArrayList<Integer>();
+	
+	BigDigit(int a, int b) {
+		setArray1(a);
+		setArray2(b);
+	}
+	
+	public void setArray1(String a) {
+		for(int i = a.length() - 1; i >= 0; i--) {
+			array1.add((int)a.charAt(i)- 48);
+		}
+	}
+	public void setArray2(String b) {
+		for(int i = b.length() - 1; i >= 0; i--) {
+			array2.add((int)b.charAt(i)- 48);
+		}		
+	}
 }
